194149120cca78e32bb481f6c5a6af259307bf88
Vincent <32320764+fanalis1@users.noreply.github.com>
2026-08-13 10:45:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>KIOS MALASNGODING</title>
+	<title>KIOS FANALIS</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="assets/js/jquery-ui/jquery-ui.css">
 	<script type="text/javascript" src="assets/js/jquery.js"></script>
